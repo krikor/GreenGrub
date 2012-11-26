7556061e83aad93a58124b000754b8220510a95e
Krikor Nigoghosian Junior <krikor.ni@gmail.com>
2012-11-26 18:07:04 -0500

    3

--- GreenGrub/GreenGrub/Site.Master.cs
+++ GreenGrub/GreenGrub/Site.Master.cs
@@ -13,5 +13,35 @@ namespace GreenGrub
         {
 
         }
+
+        protected void ImageButton2_Click(object sender, ImageClickEventArgs e)
+        {
+            Response.Redirect("Default.aspx");
+        }
+
+        protected void ImageButton3_Click(object sender, ImageClickEventArgs e)
+        {
+            Response.Redirect("LunchMenu.aspx");
+        }
+
+        protected void ImageButton4_Click(object sender, ImageClickEventArgs e)
+        {
+            Response.Redirect("OurIngredients.aspx");
+        }
+
+        protected void ImageButton5_Click(object sender, ImageClickEventArgs e)
+        {
+            Response.Redirect("About.aspx");
+        }
+
+        protected void ImageButton6_Click(object sender, ImageClickEventArgs e)
+        {
+            Response.Redirect("Contact Us.aspx");
+        }
+
+        protected void ImageButton1_Click1(object sender, ImageClickEventArgs e)
+        {
+            Response.Redirect("MyBasket.aspx");
+        }
     }
 }
