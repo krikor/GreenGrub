ac536baabe2cebac523d9ae063ce232c80df2376
Krikor Nigoghosian Junior <krikor.ni@gmail.com>
2012-11-26 17:39:02 -0500

    Tester

---
+++ GreenGrub/GreenGrub/Book.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data.OleDb;
+using System.Net;
+using System.Net.Mail;
+
+public partial class _Default : System.Web.UI.Page
+{
+   
+  
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("ConfirmationPage.aspx");
+    }
+}
